663db416ef52f44ab3dbd55f1a7de9afb53fba37
JackSloaner <96841434+JackSloaner@users.noreply.github.com>
2023-04-10 03:56:49 +0000

    Create $clearChannel command
    - Clears all messages in given channel (up to 200)
    - Logs clearing of channel in #server-logs channel

--- main.py
+++ main.py
@@ -11,17 +11,32 @@ bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 async def on_ready():
   print("ready")
 
+
 @bot.event
 async def on_message(message):
   if message.author == bot.user:
     return
-  if (not discord.utils.get(message.author.roles, name="Moderator")) and (not message.author.guild_permissions.administrator):
+  if (not discord.utils.get(message.author.roles, name="Moderator")) and (
+      not message.author.guild_permissions.administrator):
     return
 
+
 #Moderator commands
-  
+
   if message.content.startswith('$roleMenu'):
     await roleMenu(message)
+
+  if message.content.startswith('$clearChannel'): 
+    Author = message.author
+    curChannel = message.channel
+    msgs = []
+    async for message in curChannel.history(limit=200):
+      msgs.append(message)
+    await curChannel.delete_messages(msgs)
+    logChannel = discord.utils.get(message.guild.text_channels,
+                                 name="server-logs")
+    await logChannel.send("`{}` has cleared the `#{}` channel".format(
+    Author, curChannel))
 
 TOKEN = os.environ['TOKEN']
 
